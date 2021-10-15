4842f34327ab7adbb6302e2ccdfa85362cec4f09
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2021-10-15 10:36:16 +0100

    Initial tsc setup

---
+++ src/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};
